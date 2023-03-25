365fdea4d513f68dd0716c91916e3f023920376f
ABHISHEK T K <abhishektk18@gmail.com>
2023-03-25 19:12:20 +0530

    working under progress

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>welcome to header</div>
+  )
+}
+
+export default header
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDGIICbfrVRjD6c2F3huDCDy40ZNVpM9Q",
+    authDomain: "slack-advanced-workspace.firebaseapp.com",
+    projectId: "slack-advanced-workspace",
+    storageBucket: "slack-advanced-workspace.appspot.com",
+    messagingSenderId: "535789984443",
+    appId: "1:535789984443:web:6f94ebb9ebb68826f6e79e",
+    measurementId: "G-FLBSKMHF4N"
+  };
